b836b4601773f2a81f7ebfffdccb447346b48633
José Barrera <josebarrerag1996@gmail.com>
2024-04-01 11:38:15 -0300

    Introduction to Layouts & Creationg of shared components

---
+++ src/components/navbar/links/Links.js
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+
+const Links = () => {
+    const links = [
+        {
+            title: "Homepage",
+            path: "/",
+        },
+        {
+            title: "About",
+            path: "/about",
+        },
+        {
+            title: "Contact",
+            path: "/contact",
+        },
+        {
+            title: "Blog",
+            path: "/blog",
+        },
+    ];
+
+    return (
+        <div>
+            {links.map((link, index) => (
+                <Link href={link.path} key={index}>{link.title}</Link>
+            ))}
+        </div>
+    );
+}
+
+export default Links;
