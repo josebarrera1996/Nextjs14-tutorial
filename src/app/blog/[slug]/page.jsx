b7df25075d2ce0ebc625a631aa049316c1996393
José Barrera <josebarrerag1996@gmail.com>
2024-04-09 10:16:41 -0300

    Single Blog Page Design

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+import styles from "./singlePost.module.css";
 
 const SinglePostPage = () => {
+
     return (
-        <div>SinglePostPage</div>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image src="https://images.pexels.com/photos/15949261/pexels-photo-15949261/free-photo-of-edificios-cielo-azul-sombras-pueblo.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" fill className={styles.img} />
+            </div>
+            <div className={styles.textContainer}>
+                <h1 className={styles.title}>Title</h1>
+                <div className={styles.detail}>
+                    <Image className={styles.avatar} src={'/noavatar.png'} alt="" width={50} height={50} />
+                    <div className={styles.detailText}>
+                        <span className={styles.detailTitle}>Author</span>
+                        <span className={styles.detailValue}>Terry Jefferson</span>
+                    </div>
+                    <div className={styles.detailText}>
+                        <span className={styles.detailTitle}>Published</span>
+                        <span className={styles.detailValue}>01.01.2024</span>
+                    </div>
+                </div>
+                <div className={styles.content}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam illo optio nisi quo, velit eaque provident earum distinctio repellendus facere? Necessitatibus aliquid dicta suscipit quos minima explicabo facilis aperiam odio!
+                </div>
+            </div>
+        </div>
     );
 }
 
