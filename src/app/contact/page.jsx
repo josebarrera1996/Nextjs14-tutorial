6f8d4977657dbc3167f7119c0a9bfa3157b58ca6
José Barrera <josebarrerag1996@gmail.com>
2024-04-10 10:58:04 -0300

    Creating an Hydration Error

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,12 +1,18 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./contact.module.css";
 
-export const metadata = {
-    title: "Contact Page",
-    description: "Contact description",
-};
+// export const metadata = {
+//     title: "Contact Page",
+//     description: "Contact description",
+// };
 
 const ContactPage = () => {
+    // Esto hará que en el navegador se muestre un valor y en el servidor otro. Es decir, no coincidirán.
+    // Este es un ejemplo sencillo de 'Hydration Error'.
+    const a = Math.random();
+    console.log(a);
 
     return (
         <div className={styles.container}>
@@ -14,6 +20,8 @@ const ContactPage = () => {
                 <Image src={'/contact.png'} alt="" fill className={styles.img} />
             </div>
             <div className={styles.formContainer}>
+                {/* Al mostrar este valor aleatorio generará errores */}
+                Valor: {a}
                 <form action="" className={styles.form}>
                     <input type="text" placeholder="Name and Surname" />
                     <input type="text" placeholder="Email Address" />
